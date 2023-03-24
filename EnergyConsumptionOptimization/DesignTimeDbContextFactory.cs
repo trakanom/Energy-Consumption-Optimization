a25dbecbdeecb3f7ba8b39296a6be57e5c93b2ad
Mika <Mika@Wisener.dev>
2023-03-24 14:13:31 -0700

    Add DesignTimeDbContextFactory

---
+++ EnergyConsumptionOptimization/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using EnergyConsumptionOptimization.Data;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(builder.Options);
+    }
+}
