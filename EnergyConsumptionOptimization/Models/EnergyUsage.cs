276985cb786f4d2daab019e0276ced5d0bfe8c30
Mika <Mika@Wisener.dev>
2023-03-24 14:08:56 -0700

    Add EnergyUsage template

---
+++ EnergyConsumptionOptimization/Models/EnergyUsage.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EnergyConsumptionOptimization.Models
+{
+    public class EnergyUsage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Add other properties related to energy usage
+    }
+}
