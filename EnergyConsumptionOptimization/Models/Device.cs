58b399ca1dd4ab9841c2bd88154a7ecbbb792c22
Mika <Mika@Wisener.dev>
2023-03-24 15:27:36 -0700

    Update Model properties

--- EnergyConsumptionOptimization/Models/Device.cs
+++ EnergyConsumptionOptimization/Models/Device.cs
@@ -4,9 +4,23 @@ namespace EnergyConsumptionOptimization.Models
 {
     public class Device
     {
+        // Unique identifier for the device
         [Key]
         public int Id { get; set; }
 
-        // Add other properties related to devices
+        // Name of the device (e.g., "Air Conditioner")
+        [Required]
+        public string Name { get; set; }
+
+        // Type of the device (e.g., "HVAC", "Lighting", "Electronics")
+        [Required]
+        public string Type { get; set; }
+
+        // Power consumption of the device in watts
+        [Required]
+        public double PowerConsumption { get; set; }
+
+        // Additional properties can be added as needed
+        // (e.g., manufacturer, model number, installation date, etc.)
     }
 }
--- EnergyConsumptionOptimization/Models/EnergyUsage.cs
+++ EnergyConsumptionOptimization/Models/EnergyUsage.cs
@@ -1,12 +1,27 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace EnergyConsumptionOptimization.Models
 {
     public class EnergyUsage
     {
+        // Unique identifier for the energy usage entry
         [Key]
         public int Id { get; set; }
 
-        // Add other properties related to energy usage
+        // Date and time when the energy usage was recorded
+        [Required]
+        public DateTime Timestamp { get; set; }
+
+        // Reference to the related device
+        [Required]
+        public Device Device { get; set; }
+
+        // Duration of the energy usage in hours
+        [Required]
+        public double Duration { get; set; }
+
+        // Additional properties can be added as needed
+        // (e.g., location, user ID, etc.)
     }
 }
