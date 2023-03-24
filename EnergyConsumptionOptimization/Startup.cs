4a0b6641561020290370ed70e5b64eba6d91b999
Mika <Mika@Wisener.dev>
2023-03-24 14:11:53 -0700

    Update Startup template

--- EnergyConsumptionOptimization/Startup.cs
+++ EnergyConsumptionOptimization/Startup.cs
@@ -1,11 +1,30 @@
-using Microsoft.EntityFrameworkCore;
-using EnergyConsumptionOptimization.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-// ...
-
-public void ConfigureServices(IServiceCollection services)
+namespace EnergyConsumptionOptimization
 {
-    // ...
-    services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //ConfigureServices code
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure code
+        }
+    }
 }
