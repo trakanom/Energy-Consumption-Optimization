3b599e3d1bb3d4517c7f89eb16db5f11d0a1703b
Mika <Mika@Wisener.dev>
2023-03-24 14:11:24 -0700

    Add template methods to test build & migrations

--- EnergyConsumptionOptimization/Controllers/DevicesController.cs
+++ EnergyConsumptionOptimization/Controllers/DevicesController.cs
@@ -1,10 +1,59 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using EnergyConsumptionOptimization.Models;
 
-namespace EnergyConsumptionOptimization.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class Device : ControllerBase
+namespace EnergyConsumptionOptimization.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DevicesController : ControllerBase
+    {
+        // POST: api/devices
+        // Input: Device object
+        // Output: Created device record
+        // Purpose: Add a new device
+        [HttpPost]
+        public ActionResult<Device> AddDevice(Device device)
+        {
+            // Implement add device logic
+            if (ModelState.IsValid)
+            {
+                // Add the device
+                return Ok(); // or another appropriate response
+            }
+            return BadRequest(ModelState);
+        }
 
+        // PUT: api/devices/{id}
+        // Input: Device ID, updated Device object
+        // Output: Updated device record
+        // Purpose: Update a device by ID
+        [HttpPut("{id}")]
+        public ActionResult<Device> UpdateDevice(int id, Device updatedDevice)
+        {
+            // Implement update device logic
+            if (ModelState.IsValid)
+            {
+                // Add the device
+                return Ok(); // or another appropriate response
+            }
+            return BadRequest(ModelState);
+        }
+
+        // DELETE: api/devices/{id}
+        // Input: Device ID
+        // Output: None
+        // Purpose: Delete a device by ID
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDevice(int id)
+        {
+            // Implement delete device logic
+            if (ModelState.IsValid)
+            {
+                // Add the device
+                return Ok(); // or another appropriate response
+            }
+            return BadRequest(ModelState);
+        }
+    }
 }
--- EnergyConsumptionOptimization/Controllers/EnergyUsageController.cs
+++ EnergyConsumptionOptimization/Controllers/EnergyUsageController.cs
@@ -1,10 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using EnergyConsumptionOptimization.Models;
+using EnergyConsumptionOptimization.Services;
 
-namespace EnergyConsumptionOptimization.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class EnergyConsumption : ControllerBase
+namespace EnergyConsumptionOptimization.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnergyUsageController : ControllerBase
+    {
+        private readonly OptimizationService _optimizationService;
 
+        public EnergyUsageController(OptimizationService optimizationService)
+        {
+            _optimizationService = optimizationService;
+        }
+
+        // POST: api/energyusage
+        // Input: EnergyUsage object
+        // Output: Created energy usage record
+        // Purpose: Add a new energy usage record
+        [HttpPost]
+        public ActionResult<EnergyUsage> AddEnergyUsage(EnergyUsage energyUsage)
+        {
+            // Implement add energy usage logic
+            if (ModelState.IsValid)
+            {
+                // Add the device
+                return Ok(); // or another appropriate response
+            }
+            return BadRequest(ModelState);
+        }
+
+        // GET: api/energyusage
+        // Input: None
+        // Output: List of energy usage records
+        // Purpose: Retrieve all energy usage records
+        [HttpGet]
+        public ActionResult<IEnumerable<EnergyUsage>> GetAllEnergyUsage()
+        {
+            // Implement get all energy usage logic
+            if (ModelState.IsValid)
+            {
+                // Add the device
+                return Ok(); // or another appropriate response
+            }
+            return BadRequest(ModelState);
+        }
+
+        // GET: api/energyusage/recommendations
+        // Input: None
+        // Output: List of recommendations
+        // Purpose: Calculate and retrieve recommendations based on energy usage records
+        [HttpGet("recommendations")]
+        public ActionResult<IEnumerable<Recommendations>> GetRecommendations()
+        {
+            // Implement get recommendations logic
+            if (ModelState.IsValid)
+            {
+                // Add the device
+                return Ok(); // or another appropriate response
+            }
+            return BadRequest(ModelState);
+        }
+    }
 }
