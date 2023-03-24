bb47d24fbe5a8f3b9e1b5eac49417b4e212062eb
Mika <Mika@Wisener.dev>
2023-03-24 14:35:06 -0700

    Begin CRUD configuration in service layer

--- EnergyConsumptionOptimization/Services/OptimizationService.cs
+++ EnergyConsumptionOptimization/Services/OptimizationService.cs
@@ -1,7 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using EnergyConsumptionOptimization.Models;
+using EnergyConsumptionOptimization.Data;
+
 namespace EnergyConsumptionOptimization.Services
 {
     public class OptimizationService
     {
-        //Optimization Service code here
+        private readonly ApplicationDbContext _context;
+
+        public OptimizationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<EnergyUsage> GetAllEnergyUsages()
+        {
+            return _context.EnergyUsages.ToList();
+        }
+
+        public void AddEnergyUsage(EnergyUsage energyUsage)
+        {
+            _context.EnergyUsages.Add(energyUsage);
+            _context.SaveChanges();
+        }
+
+        public List<Device> GetAllDevices()
+        {
+            return _context.Devices.ToList();
+        }
+
+        public void AddDevice(Device device)
+        {
+            _context.Devices.Add(device);
+            _context.SaveChanges();
+        }
+
+        // Add other CRUD methods for updating and deleting EnergyUsage and Device entities
+
+        // Add optimization logic related to energy consumption here
     }
 }
