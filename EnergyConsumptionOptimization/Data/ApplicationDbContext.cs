1baca0710ceb28e231b19ed20bede86149cc2918
Mika <Mika@Wisener.dev>
2023-03-24 14:10:49 -0700

    Update ApplicationDbContext to inherit DbContext and properties

--- EnergyConsumptionOptimization/Data/ApplicationDbContext.cs
+++ EnergyConsumptionOptimization/Data/ApplicationDbContext.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using EnergyConsumptionOptimization.Models;
+
 namespace EnergyConsumptionOptimization.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
+        public DbSet<EnergyUsage> EnergyUsages { get; set; }
+        public DbSet<Device> Devices { get; set; }
     }
 }
