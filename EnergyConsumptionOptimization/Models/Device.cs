6a3acd7e9093e3e15634f3a63a05bb5defc906e4
Mika <Mika@Wisener.dev>
2023-03-24 14:09:59 -0700

    Add public key to Device template

--- EnergyConsumptionOptimization/Models/Device.cs
+++ EnergyConsumptionOptimization/Models/Device.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EnergyConsumptionOptimization.Models
 {
     public class Device
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        // Add other properties related to devices
     }
 }
